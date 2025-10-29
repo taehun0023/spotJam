f83230067c1612e70a034e22a221cb8978f16c5e
taehun <taehun0023@gmail.com>
2025-10-29 12:21:37 +0900

    frist commit

---
+++ backend/src/main/java/com/spotjam/backend/SpotjamApplication.java
@@ -0,0 +1,13 @@
+package com.spotjam.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotjamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotjamApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/spotjam/backend/SpotjamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spotjam.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotjamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
